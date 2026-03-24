d2255bb71e22a6e92ae85ef312301ba766bf815b
kyliandambrosio <kylian.dambrosio@gmail.com>
2026-03-24 18:29:13 +0100

    Update GestBdd.js

--- GestBdd.js
+++ GestBdd.js
@@ -1,17 +1,19 @@
-/////////////////////////////////////////////////SAVE GOOGLE SHEET//////////////////////////
+/////////////////////////////////////////////ECHANGE GOOGLE SHEET//////////////////////////
 const URL = "https://script.google.com/macros/s/AKfycbxxTlkL6Wl3UOpp6MIsasdx-LdfoUnM1JnjE0DmTcSU_s2Oqk9BHKeDBieN5IsZLenO/exec";
 const AddCig = document.getElementById("Bp_AddCig");
 const Cpt_CigJour = document.getElementById("Cpt_CigJour");
 
-export async function SaveDataInGoogleSheets(action, cellule, valeur) {
+///////////////////////////////////////////////////////////////////////////////////////////
+//Read From Google Sheets
+export async function ReadataInGoogleSheets() {
   //Construction Data
   const data = {
-  action: action, //read / write
-  cellule: cellule,
-	valeur: valeur,
+  action: "read",
+  cellule: 0,
+	valeur: 0,
   };
 
-  console.log("Access In GoogleSheets IN PROGRESS",data)
+  console.log("Read In GoogleSheets IN PROGRESS",data)
 
   //Request data
   return fetch(URL, {
@@ -19,13 +21,45 @@ export async function SaveDataInGoogleSheets(action, cellule, valeur) {
     body: JSON.stringify(data)
   })
 
-  .then(response => response.text())
-  .then (valeurRetour => {
-      console.log("Access In GoogleSheets OK ||", valeurRetour)
-      return valeurRetour
+  .then(res => res.json())
+  .then (data => {
+      console.log("Read In GoogleSheets OK ||", data)
+      return data
   })
   .catch(error => {
-      console.log("Access In GoogleSheets FAILLED",data)
+      console.log("Read In GoogleSheets FAILLED",data)
+  });
+
+
+
+};
+ 
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//Write To Google Sheets
+export async function SaveDataInGoogleSheets(action, cellule, valeur) {
+  //Construction Data
+  const data = {
+  action: "write",
+  cellule: cellule,
+	valeur: valeur,
+  };
+
+  console.log("Write In GoogleSheets IN PROGRESS",data)
+
+  //Request data
+  return fetch(URL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+
+  .then(res => res.text())
+  .then (Valeur => {
+      console.log("Write In GoogleSheets OK")
+      return Valeur
+  })
+  .catch(error => {
+      console.log("Write In GoogleSheets FAILLED")
   });
 
 
